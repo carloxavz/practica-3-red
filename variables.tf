d0495f8eb3f755d8522780d1db2be26b82c6b9da
carloxavz <carloxavz>
2026-09-14 21:41:45 +0000

    fase 1: vpc y subred publica

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 variable "proyecto"     { type = string }
 variable "prefijo"      { type = string }
-variable "region"       { type = string  default = "us-central1" }
-variable "cidr_publica" { type = string  default = "10.10.1.0/24" }
+variable "region"       {
+ type = string 
+ default = "us-central1"
+ }
+variable "cidr_publica" {
+ type = string 
+ default = "10.10.1.0/24"
+ }
