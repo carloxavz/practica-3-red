f63d9b372861fbdd2192ec9485f1c81475cac395
carloxavz <carloxavz>
2026-09-14 21:34:43 +0000

    Configuracion inicial, main tf, varialbes tf, etc

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = { source = "hashicorp/google" }
+  }
+}
+
+provider "google" {
+  project = var.proyecto
+  region  = var.region
+}
+
+# La VPC es global. En modo personalizado nace sin subredes.
+resource "google_compute_network" "vpc" {
+  name                    = "${var.prefijo}-vpc"
+  auto_create_subnetworks = false
+}
+
+# La subred sí es regional, y es donde las máquinas toman su IP interna.
+resource "google_compute_subnetwork" "publica" {
+  name          = "${var.prefijo}-sub-publica"
+  ip_cidr_range = var.cidr_publica
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
